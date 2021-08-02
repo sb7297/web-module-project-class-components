0ff45bec87dc42a665333472d35447473bdaece5
sb7297 <sbaireddy7297@gmail.com>
2021-08-02 14:02:49 -0400

    App.js: define state

--- src/App.js
+++ src/App.js
@@ -4,11 +4,30 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          task: "Vacuum the kitchen",
+          id: Date.now() - 10,
+          completed: true
+        },
+        {
+          task: "Balance binary tree",
+          id: Date.now() - 20,
+          completed: false
+        }
+      ]
+    };
+  }
+
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
-      </div>
+      this.state.todos.map(todo => {
+        return <div>{todo.task} {todo.id} {todo.completed ? "DONE" : "TODO"}</div>
+      })
     );
   }
 }
